d407a9f71ecc49ca6d85e6e7a8620ba9a1f7307d
wootloyalty <woot.loyalty@gmail.com>
2017-05-20 21:13:08 -0400

    se corrige la configuracion de las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'home/show'
-
-  get 'login/index'
-
-  root 'login#index'
-end
-
-
-GoogleAuthExample::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
