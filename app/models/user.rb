4c591134cd4cec4e455fd794e39663e2fc6e12e8
wootloyalty <woot.loyalty@gmail.com>
2017-05-20 21:36:03 -0400

    corrección por el encabezado de como se declaraba el modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord::Base
+class User < ActiveRecord::Base
 	def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
