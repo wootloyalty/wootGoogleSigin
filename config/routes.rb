a8076c5bbd47abbeb0288a51b7354062d540d6f4
wootloyalty <woot.loyalty@gmail.com>
2017-05-20 20:14:50 -0400

    Configuracion de las rutas para manejar los controladores

--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,15 @@ Rails.application.routes.draw do
 
   root 'login#index'
 end
+
+
+GoogleAuthExample::Application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#show"
+end
